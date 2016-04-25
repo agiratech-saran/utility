763be318442455d20e326784879e002e6cd826cc
Agiratech.saravana <saran@agiratech.com>
2016-04-25 16:53:52 +0530

    Array reverse

---
+++ array/array.go
@@ -0,0 +1,13 @@
+package array
+
+// import "fmt"
+
+func ReverseArray(input []int) []int {
+
+    for i, j := 0, len(input)-1; i < j; i, j = i+1, j-1 {
+        input[i], input[j] = input[j], input[i]
+    }
+
+    return input
+}
+
